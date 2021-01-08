8c0ffee7c9b20062dd6e46d2d218f857465a5475
mohamedelhafy <40215551+mohamedelhafy@users.noreply.github.com>
2021-01-08 23:37:31 +0200

    Update Calc_project.asm

--- Calc_project.asm
+++ Calc_project.asm
@@ -10,12 +10,12 @@
 	.data
 		MAX = 100
 		stringIn BYTE MAX+1 DUP(?), 0
-    SYM_ADD BYTE '+'
+   		SYM_ADD BYTE '+'
 		SYM_SUB BYTE '-'
 		SYM_MUL BYTE '*'
 		SYM_DIV BYTE '/'
-    CURR_SYM_ADD BYTE '+'
+    		CURR_SYM_ADD BYTE '+'
 		CURR_SYM_MUL BYTE '*'
-    RES_ADD DWORD 0
+    		RES_ADD DWORD 0
 		RES_MUL DWORD 1
 		PARSE_RES DWORD 0
